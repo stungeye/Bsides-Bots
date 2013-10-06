d50a1e079ec3d214f6b62d5c79de8b32fb78e39d
Wally Glutton <stungeye@gmail.com>
2013-10-06 14:24:23 -0500

    The Google Lucky Bot (@lmgtfyb) in it's current live state.

--- lmgtfy/lmgtfyb.rb
+++ lmgtfy/lmgtfyb.rb
@@ -1,18 +1,34 @@
 require 'chatterbot/dsl'
 require 'google-search'
 
-no_update
-verbose
+# Twitter will shorten URLS to 23 character and we'll be adding a
+# space before the URL so the max length is 140 - 23 - 1 = 116.
+MAX_TWEET_LENGTH = 116
 
-# tweet "Hello world. I'm LMGTFY Bot, I Google things for you."
+# The bot name plus a space needs to be striped from the received tweets
+# to determine the search query.
+BOT_NAME = '@lmgtfyb '
+
+# If we truncate a title we'll put this at the end of the string.
+TRUNCATION_INDICATOR = '...'
+
+# Remove these lines once we start running this via cron.
+#no_update
+#verbose
 
 replies do |tweet|
-  google_query = tweet.text.gsub(/@lmgtfyb /, '')
-  feeling_lucky = Google::Search::Web.new(:query => google_query).first
-  feeling_lucky_uri = feeling_lucky.uri
-  feeling_lucky_title = feeling_lucky.title
-  # Need to take the tweet_user's name length into consideration here:
-  feeling_lucky_title = feeling_lucky_title.length < 117 ? 
-    feeling_lucky_title : feeling_lucky_title.slice(0..113) + "..."
-  puts "#{tweet_user(tweet)} #{feeling_lucky_title} #{feeling_lucky_uri}"
+  user_who_tweeted = tweet_user(tweet)
+  
+  google_query_received = tweet.text.gsub(BOT_NAME, '')
+  feeling_lucky = Google::Search::Web.new(:query => google_query_received).first
+  
+  response_with_at_reply = "#{user_who_tweeted} #{feeling_lucky.title}"
+  
+  if response_with_at_reply.length > MAX_TWEET_LENGTH
+    response_with_at_reply = response_with_at_reply[0...(MAX_TWEET_LENGTH-TRUNCATION_INDICATOR.size)] + TRUNCATION_INDICATOR
+  end
+  
+  full_response_tweet = "#{response_with_at_reply} #{feeling_lucky.uri}"
+  
+  tweet full_response_tweet
 end
