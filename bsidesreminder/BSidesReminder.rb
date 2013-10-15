b5c5d99bd1877f8f896ea404c8d9a6915f41933b
Wally Glutton <stungeye@gmail.com>
2013-10-14 20:33:30 -0500

    Finished the Bsides Reminder Bot.

--- bsidesreminder/BSidesReminder.rb
+++ bsidesreminder/BSidesReminder.rb
@@ -1,14 +1,22 @@
-require 'chatterbot/dsl'
 require 'date'
 
-# remove this to send out tweets
-debug_mode
-
-# remove this to update the db
-no_update
-# remove this to get less output when running
-verbose
-
 BSIDES_WINNIPEG_2013_DATE = Date.parse('November 16 2013')
+days_until_bsides = (BSIDES_WINNIPEG_2013_DATE - Date.today).to_i
 
-puts BSIDES_WINNIPEG_2013_DATE
+if days_until_bsides < 0 
+  exit
+end
+
+require 'chatterbot/dsl'
+
+tweet_text = "@stungeye You are presenting @BSidesWpg "
+
+if days_until_bsides.zero?
+  tweet_text += "today!"
+elsif days_until_bsides == 1
+  tweet_text += "tomorrow."
+else
+  tweet_text += "in #{days_until_bsides} days."
+end
+
+tweet tweet_text
