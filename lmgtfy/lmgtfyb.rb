3133dc59d92d8e726c401db562325f72f7e521b0
Wally Glutton <stungeye@gmail.com>
2013-11-02 12:45:08 -0500

    Fixed a bug with lmgtfyb when search results were nil.

--- lmgtfy/lmgtfyb.rb
+++ lmgtfy/lmgtfyb.rb
@@ -1,5 +1,6 @@
 require 'chatterbot/dsl'
 require 'google-search'
+require 'cgi'
 
 # Twitter will shorten URLS to 23 character and we'll be adding a
 # space before the URL so the max length is 140 - 23 - 1 = 116.
@@ -12,25 +13,35 @@ BOT_NAME = '@lmgtfyb '
 # If we truncate a title we'll put this at the end of the string.
 TRUNCATION_INDICATOR = '...'
 
+NO_RESULTS_FOUND = 'Ever so sorry; I found no websites that reference: '
+
 # Remove these lines once we start running this via cron.
+#debug_mode
 #no_update
 #verbose
 
-replies do |tweet|
-  user_who_tweeted = tweet_user(tweet)
+replies do |user_tweet|
+  user_who_tweeted = tweet_user(user_tweet)
   
-  google_query_received = tweet.text.gsub(BOT_NAME, '')
+  google_query_received = user_tweet.text.gsub(/[#\@]\w+\b/,'').strip
+  
   feeling_lucky = Google::Search::Web.new(:query => google_query_received).first
-  
-  response_with_at_reply = "#{user_who_tweeted} #{feeling_lucky.title}"
-  
-  if response_with_at_reply.length > MAX_TWEET_LENGTH
-    response_with_at_reply = response_with_at_reply[0...(MAX_TWEET_LENGTH-TRUNCATION_INDICATOR.size)] + TRUNCATION_INDICATOR
+
+  response_tweet = user_who_tweeted + " "
+
+  if feeling_lucky.nil?
+    response_tweet += NO_RESULTS_FOUND + google_query_received 
+  else
+    response_tweet += CGI.unescapeHTML(feeling_lucky.title)
+  end
+    
+  if response_tweet.length > MAX_TWEET_LENGTH
+    response_tweet = response_tweet[0...(MAX_TWEET_LENGTH-TRUNCATION_INDICATOR.size)] + TRUNCATION_INDICATOR
   end
   
-  full_response_tweet = "#{response_with_at_reply} #{feeling_lucky.uri}"
-  
-  reply full_response_tweet, tweet
+  response_tweet += " " + feeling_lucky.uri  unless feeling_lucky.nil?
+
+  reply response_tweet, user_tweet
   
   sleep 1
 end
