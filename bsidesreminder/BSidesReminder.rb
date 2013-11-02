8721a9477d98be677cc8966a5d9cf32ae7100bd2
Wally Glutton <stungeye@gmail.com>
2013-11-02 13:00:26 -0500

    Added comments to the bsides reminder bot.

--- bsidesreminder/BSidesReminder.rb
+++ bsidesreminder/BSidesReminder.rb
@@ -1,15 +1,17 @@
 require 'date'
 
+# Determine the number of days until the conference.
 BSIDES_WINNIPEG_2013_DATE = Date.parse('November 16 2013')
 days_until_bsides = (BSIDES_WINNIPEG_2013_DATE - Date.today).to_i
 
+# Leave this script if the conference is over.
 if days_until_bsides < 0 
   exit
 end
 
-sleep 30
-
 require 'chatterbot/dsl'
+
+# Build the tweet and send it.
 
 tweet_text = "@stungeye You are presenting @BSidesWpg "
 
